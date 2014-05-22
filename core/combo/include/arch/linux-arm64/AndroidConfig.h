74eab88a7762c67a36d5ada49b8b3cd8c7952524
Shashank Mittal <mittals@codeaurora.org>
2014-05-21 17:36:49 -0700

    Define CONFIG_64BIT for LP64 target
    Linux kernel uapi headers use CONFIG_64BIT flag. We need to define that if
    __LP64__ flag is defined.
    
    Change-Id: I2143ca8d91d70506485f3eb6f21a9e5243c495cc
    Signed-off-by: klozz <xxx.reptar.rawrr.xxx@gmail.com>

--- core/combo/include/arch/linux-arm64/AndroidConfig.h
+++ core/combo/include/arch/linux-arm64/AndroidConfig.h
@@ -52,4 +52,46 @@
  */
 #define HAVE_STDINT_H 1
 
+/*
+ * Define if <stdbool.h> exists.
+ */
+#define HAVE_STDBOOL_H 1
+
+/*
+ * Define if <sched.h> exists.
+ */
+#define HAVE_SCHED_H 1
+
+/*
+ * Define if pread() exists
+ */
+#define HAVE_PREAD 1
+
+/*
+ * Define if we have st_mtim in struct stat
+ */
+#define HAVE_STAT_ST_MTIM 1
+
+/*
+ * Define if printf() supports %zd for size_t arguments
+ */
+#define HAVE_PRINTF_ZD 1
+
+/*
+ * Define to 1 if <stdlib.h> provides qsort_r() with a BSD style function prototype.
+ */
+#define HAVE_BSD_QSORT_R 0
+
+/*
+ * Define to 1 if <stdlib.h> provides qsort_r() with a GNU style function prototype.
+ */
+#define HAVE_GNU_QSORT_R 0
+
+/*
+ * Define CONFIG_64BIT based on __LP64__
+ */
+#ifdef __LP64__
+#define  CONFIG_64BIT
+#endif
+
 #endif /* _ANDROID_CONFIG_H */

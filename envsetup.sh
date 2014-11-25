9dbbe985bd9b9474dd80e053caa541bc9d632b39
klozz <xxx.reptar.rawrr.xxx@gmail.com>
2014-11-25 17:57:19 -0600

    XPerience paths
    Change-Id: I2bef5fca619c21531c04197a7c270ffc682724f4
    Signed-off-by: klozz <xxx.reptar.rawrr.xxx@gmail.com>

--- envsetup.sh
+++ envsetup.sh
@@ -74,8 +74,8 @@ function check_product()
         return
     fi
 
-    if (echo -n $1 | grep -q -e "^cm_") ; then
-       CM_BUILD=$(echo -n $1 | sed -e 's/^cm_//g')
+    if (echo -n $1 | grep -q -e "^xpe_") ; then
+       CM_BUILD=$(echo -n $1 | sed -e 's/^xpe_//g')
        export BUILD_NUMBER=$((date +%s%N ; echo $CM_BUILD; hostname) | openssl sha1 | sed -e 's/.*=//g; s/ //g' | cut -c1-10)
     else
        CM_BUILD=
@@ -536,7 +536,7 @@ function breakfast()
     CM_DEVICES_ONLY="true"
     unset LUNCH_MENU_CHOICES
     add_lunch_combo full-eng
-    for f in `/bin/ls vendor/cm/vendorsetup.sh 2> /dev/null`
+    for f in `/bin/ls vendor/XPe/vendorsetup.sh 2> /dev/null`
         do
             echo "including $f"
             . $f
@@ -556,7 +556,7 @@ function breakfast()
             if [ -z "$variant" ]; then
                 variant="userdebug"
             fi
-            lunch cm_$target-$variant
+            lunch xpe_$target-$variant
         fi
     fi
     return $?
@@ -709,7 +709,7 @@ function eat()
 {
     if [ "$OUT" ] ; then
         MODVERSION=$(get_build_var CM_VERSION)
-        ZIPFILE=XPerience-$MODVERSION.zip
+        ZIPFILE=xpe_$MODVERSION.zip
         ZIPPATH=$OUT/$ZIPFILE
         if [ ! -f $ZIPPATH ] ; then
             echo "Nothing to eat"
@@ -2293,7 +2293,7 @@ unset f
 
 # Add completions
 check_bash_version && {
-    dirs="sdk/bash_completion vendor/cm/bash_completion"
+    dirs="sdk/bash_completion vendor/XPe/bash_completion"
     for dir in $dirs; do
     if [ -d ${dir} ]; then
         for f in `/bin/ls ${dir}/[a-z]*.bash 2> /dev/null`; do

8124e17ac4e218c75eba4c25df80ede714d97444
klozz <xxx.reptar.rawrr.xxx@gmail.com>
2015-10-11 16:32:31 -0500

    build: Preliminary support for XPE product builds If building a xpe_* product, skip crawling the filesystem for AndroidProduct makefiles (and, of course, parsing their contents) and aim directly for the device/xpe/<product> device configuration
    Change-Id: I2a5e70dda973a6fcdbba0d5e26b35b99d3f1aea2
    
    Change how we search for XPE makefiles.
    
    build: Fix fastpath code for XPE_BUILD
    
     * We need to adjust to the new validations
    
    Change-Id: I9066d4f437beb9597027ee6bbb52504b5e7e84c5
    
    build: Fix xpe builds
    
    the setup variable wasn't exported, and was being missed by part
    of the scripts
    
    Signed-off-by: klozz <xxx.reptar.rawrr.xxx@gmail.com>

--- envsetup.sh
+++ envsetup.sh
@@ -68,6 +68,14 @@ function check_product()
         echo "Couldn't locate the top of the tree.  Try setting TOP." >&2
         return
     fi
+
+    if (echo -n $1 | grep -q -e "^xpe_") ; then
+       XPE_BUILD=$(echo -n $1 | sed -e 's/^xpe_//g')
+    else
+       XPE_BUILD=
+    fi
+    export XPE_BUILD
+
         TARGET_PRODUCT=$1 \
         TARGET_BUILD_VARIANT= \
         TARGET_BUILD_TYPE= \

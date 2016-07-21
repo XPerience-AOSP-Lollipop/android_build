027ae6dd70eee49e851522c93a1ebc931e4d6155
Sagar Shah <sagarshah2326@gmail.com>
2016-07-21 22:44:33 +0530

    Commit to make Xperience compatible with OpenJDK 1.7..

--- core/main.mk
+++ core/main.mk
@@ -184,9 +184,9 @@ endif
 # Check for the current JDK.
 #
 # For Java 1.7, we require OpenJDK on linux and Oracle JDK on Mac OS.
-requires_openjdk := false
+requires_openjdk := true
 ifeq ($(HOST_OS), linux)
-requires_openjdk := false
+requires_openjdk := true
 endif
 
 

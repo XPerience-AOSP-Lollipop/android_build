851ab5d3da1e48ff67faa086b29f63b66c2ff8d2
klozz <xxx.reptar.rawrr.xxx@gmail.com>
2017-02-18 17:38:55 -0600

    Revert "for now Revert "Revert "Add PRODUCT_BOOTANIMATION"""
    Signed-off-by: klozz <xxx.reptar.rawrr.xxx@gmail.com>

--- core/product_config.mk
+++ core/product_config.mk
@@ -357,6 +357,13 @@ endif
 PRODUCT_COPY_FILES := \
     $(strip $(PRODUCTS.$(INTERNAL_PRODUCT).PRODUCT_COPY_FILES))
 
+_boot_animation := $(strip $(lastword $(PRODUCTS.$(INTERNAL_PRODUCT).PRODUCT_BOOTANIMATION)))
+ifneq ($(_boot_animation),)
+PRODUCT_COPY_FILES += \
+    $(_boot_animation):system/media/bootanimation.zip
+endif
+_boot_animation :=
+
 # We might want to skip items listed in PRODUCT_COPY_FILES for
 # various reasons. This is useful for replacing a binary module with one
 # built from source. This should be a list of destination files under $OUT
